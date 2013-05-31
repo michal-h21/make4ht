88a4473ded7c9fc4c053bed5d709fe8b8a5c1d9b
Michal Hoftich <michal.h21@gmail.com>
2013-05-31 15:11:40 +0200

    Added path patter matching, copying files

--- make4ht-lib.lua
+++ make4ht-lib.lua
@@ -5,6 +5,9 @@ module("make4ht",package.seeall)
 Make = {}
 --Make.params = {}
 Make.build_seq = {}
+-- Patterns for matching output filenames
+Make.matches = {}
+
 Make.add = function(self,name,fn,par)
 	local par = par or {}
 	local params = self.params or {}
@@ -30,19 +33,92 @@ end
 Make.length = function(self)
 	return #self.build_seq
 end
+
+Make.match = function(self, pattern, command, params) 
+	local params = params or {}
+  self.matches[pattern] = {command = command, params = params}
+end
+
+Make.run_command = function(self,filename,s)
+	local command = s.command
+	local params  = s.params
+	params["filename"] = filename
+	print("parse_lg process file: "..filename)
+	--for k,v in pairs(params) do print(k..": "..v) end
+	if type(command) == "function" then
+		return command(filename,params)
+	elseif type(command) == "string" then
+		local run = command % params
+		print("Execute: " .. run)
+    return os.execute(run)
+	end
+	return false, "parse_lg: Command is not string or function"
+end
+
+
+Make.image_convert =  function(self, images)
+end
+
+Make.file_matches = function(self, files)
+	local statuses = {}
+	-- First make params for all matchers
+	for k,v in pairs(self.matches) do
+		local v = self.matches[k]
+		local p = self.params or {}
+		for i,k in pairs(v.params) do
+			p[i] = k
+		end
+		self.matches[k].params = p
+	end
+	-- Loop over files, run command on matched
+	for _, file in pairs(files)do
+		statuses[file] = {}
+		for pattern, s in pairs(self.matches) do
+			if file:match(pattern) then 
+				local status, msg = self:run_command(file,s)
+				msg = msg or "No message given"
+				table.insert(statuses[file],status)
+				if status == false then
+					print(msg)
+					break
+				end
+			end
+		end
+	end
+	return statuses
+end
+
 Make.run = function(self) 
 	local return_codes = {}
+  local params = self.params or {}
 	for _,v in ipairs(self.build_seq) do
 		--print("sekvence: "..v.name)
-	  local params = self.params or {}
 		for p,n in pairs(v.params) do params[p] = n end
 		--for c,_ in pairs(params) do print("build param: "..c) end
-		local command = v.command % params
-		print("Make4ht: " .. command)
-		local status = os.execute(command)
-		table.insert(return_codes,{name=v.name,status=status})
+		if type(v.command)=="function" then 
+			table.insert(return_codes,{name=v.name,status = v.command(params)})
+		elseif type(v.command) =="string" then
+			local command = v.command % params
+			print("Make4ht: " .. command)
+			local status = os.execute(command)
+			table.insert(return_codes,{name=v.name,status=status})
+		else
+			print("Unknown command type, must be string or function - " ..v.name..": "..type(v.command))
+		end
 	end
-  return return_codes
+	local lgfile = params.input and params.input .. ".lg" or nil 
+	if lgfile then
+   	local lg = mkutils.parse_lg(lgfile)
+		-- First convert images from lg files
+		self:image_convert(lg["images"])
+		-- Then run file matchers on lg files and converted images
+		local files = lg["files"]
+		for _,v in pairs(lg["images"]) do table.insert(files,v) end
+		self:file_matches(files)
+	else
+		print("No lg file. tex4ht run failed?")
+	end
+	return return_codes
 end
 
 --[[Make:add("hello", "hello ${world}", {world = "world"})
--- mkutils.lua
+++ mkutils.lua
@@ -4,16 +4,16 @@ local make4ht = require("make4ht")
 --template engine
 function interp(s, tab)
 	local tab = tab or {}
-  return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
+	return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
 end
 --print( interp("${name} is ${value}", {name = "foo", value = "bar"}) )
 
 function addProperty(s,prop)
-  if prop ~=nil then
-    return s .." "..prop
-  else
-    return s
-  end
+	if prop ~=nil then
+		return s .." "..prop
+	else
+		return s
+	end
 end
 getmetatable("").__mod = interp
 getmetatable("").__add = addProperty 
@@ -22,10 +22,10 @@ getmetatable("").__add = addProperty
 -- Outputs "foo is bar"
 
 function string:split(sep)
-  local sep, fields = sep or ":", {}
-  local pattern = string.format("([^%s]+)", sep)
-  self:gsub(pattern, function(c) fields[#fields+1] = c end)
-  return fields
+	local sep, fields = sep or ":", {}
+	local pattern = string.format("([^%s]+)", sep)
+	self:gsub(pattern, function(c) fields[#fields+1] = c end)
+	return fields
 end
 
 function remove_extension(path)
@@ -40,31 +40,31 @@ end
 -- 
 
 function file_exists(file)
-  local f = io.open(file, "rb")
-  if f then f:close() end
-  return f ~= nil
+	local f = io.open(file, "rb")
+	if f then f:close() end
+	return f ~= nil
 end
 
 -- searching for converted images
 function parse_lg(filename)
-  print("Parse LG")
-  local outputimages,outputfiles,status={},{},nil
-  if not file_exists(filename) then
-    print("Cannot read log file: "..filename)
-  else
-    local usedfiles={}
-    for line in io.lines(filename) do
-      line:gsub("==> ([%a%d%p%.%-%_]*)",function(k) table.insert(outputimages,k)end)
-      line:gsub("File: (.*)",  function(k) 
-	 if not usedfiles[k] then
-	   table.insert(outputfiles,k)
-	   usedfiles[k] = true
-         end
-      end)
-    end
-    status=true
-  end
-  return {files = outputfiles, images = outputimages},status
+	print("Parse LG")
+	local outputimages,outputfiles,status={},{},nil
+	if not file_exists(filename) then
+		print("Cannot read log file: "..filename)
+	else
+		local usedfiles={}
+		for line in io.lines(filename) do
+			line:gsub("==> ([%a%d%p%.%-%_]*)",function(k) table.insert(outputimages,k)end)
+			line:gsub("File: (.*)",  function(k) 
+				if not usedfiles[k] then
+					table.insert(outputfiles,k)
+					usedfiles[k] = true
+				end
+			end)
+		end
+		status=true
+	end
+	return {files = outputfiles, images = outputimages},status
 end
 
 --function copy(src,dest, filter)
@@ -77,56 +77,85 @@ end
 --end
 
 local cp_func = os.type == "unix" and "cp" or "copy"
-function copy(src,dest)
+function cp(src,dest)
 	local command = string.format("%s %s %s", cp_func, src, dest)
 	if cp_func == "copy" then command = command:gsub("/",'\\') end
 	print("Copy: "..command)
 	os.execute(command)
 end
 
+local used_dir = {}
+function copy(filename,outfilename)
+	local parts = outfilename:split("/")
+	if type(parts) == "table" then
+		table.remove(parts,#parts)
+		local path = table.concat(parts,"/")
+		if not used_dir[path] then
+			print("Path to be created: "..path)
+			mkdirectories(parts)
+		end
+		used_dir[path]=true
+	end
+	if filename ~= outfilename then cp(filename,outfilename) end
+end
 mkdirectories = function(dirs)
-  local currdir = lfs.currentdir()
-  --print("Path: "..path) 
-  --local dirs = path:split("/")
-  --table.remove(dirs,#dirs) 
-  for _,dir in ipairs(dirs) do
-    local status = lfs.chdir(dir)
-    if not status then  
-      local status, msg = lfs.mkdir(dir)
-      if not status then 
-        print("Path making error: "..msg)
-        break
-      else
-        lfs.chdir(dir)
-      end
-    end
-    print(dir)
-  end
-  --lfs.copy(currdir.."/".."make.lua",currdir.."/"..path.."/make.lua")
-  lfs.chdir(currdir)
-  --os.execute("cp make.lua "..path)
+	local currdir = lfs.currentdir()
+	--print("Path: "..path) 
+	--local dirs = path:split("/")
+	--table.remove(dirs,#dirs) 
+	for _,dir in ipairs(dirs) do
+		local status = lfs.chdir(dir)
+		if not status then  
+			local status, msg = lfs.mkdir(dir)
+			if not status then 
+				print("Path making error: "..msg)
+				break
+			else
+				lfs.chdir(dir)
+			end
+		end
+		--print(dir)
+	end
+	--lfs.copy(currdir.."/".."make.lua",currdir.."/"..path.."/make.lua")
+	lfs.chdir(currdir)
+	--os.execute("cp make.lua "..path)
 end
 
 
 -- Config loading
 local function run(untrusted_code, env)
-  if untrusted_code:byte(1) == 27 then return nil, "binary bytecode prohibited" end
-  local untrusted_function, message = loadstring(untrusted_code)
-  if not untrusted_function then return nil, message end
-  setfenv(untrusted_function, env)
-  return pcall(untrusted_function)
+	if untrusted_code:byte(1) == 27 then return nil, "binary bytecode prohibited" end
+	local untrusted_function, message = loadstring(untrusted_code)
+	if not untrusted_function then return nil, message end
+	setfenv(untrusted_function, env)
+	return pcall(untrusted_function)
 end
 
 local main_settings = {}
 main_settings.fonts = {}
 local env = {}
-env.Font = function(s)
-  local font_name = s["name"]
-  if not font_name then return nil, "Cannot find font name" end
-  env.settings.fonts[font_name] = s
+
+-- We make sandbox for make script, all functions must be explicitely declared
+-- Function declarations:
+env.pairs  = pairs
+env.ipairs = ipairs
+env.print  = print
+env.split  = split
+env.string = string
+env.table  = table
+env.copy   = copy
+env.mkdirectories = mkdirectories
+env.texio  = texio
+env.type   = type
+env.lfs    = lfs
+env.os     = os
+env.Font   = function(s)
+	local font_name = s["name"]
+	if not font_name then return nil, "Cannot find font name" end
+	env.settings.fonts[font_name] = s
 end
 
-env.Make = make4ht.Make
+env.Make   = make4ht.Make
 env.Make.params = env.settings
 env.Make:add("test","no takže ${tex4ht_sty_par} ${htlatex} ${input} ${config}")
 --env.Make:add("htlatex", "${htlatex} ${latex_par} '\\\makeatletter\\def\\HCode{\\futurelet\\HCode\\HChar}\\def\\HChar{\\ifx\"\\HCode\\def\\HCode\"##1\"{\\Link##1}\\expandafter\\HCode\\else\\expandafter\\Link\\fi}\\def\\Link#1.a.b.c.{\\g@addto@macro\\@documentclasshook{\\RequirePackage[#1,html]{tex4ht}\\let\\HCode\\documentstyle\\def\\documentstyle{\\let\\documentstyle\\HCode\\expandafter\\def\\csname tex4ht\\endcsname{#1,html}\\def\\HCode####1{\\documentstyle[tex4ht,}\\@ifnextchar[{\\HCode}{\\documentstyle[tex4ht]}}}\\makeatother\\HCode '${config}${tex4ht_sty_par}'.a.b.c.\\input ' ${input}")
@@ -135,14 +164,14 @@ env.Make:add("tex4ht","tex4ht ${input} ${tex4ht_par}")
 env.Make:add("t4ht","t4ht ${input} ${t4ht_par}")
 
 function load_config(settings, config_name)
-  local settings = settings or main_settings
-  env.settings = settings
-  local config_name = config_name or "config.lua"
-  local f = io.open(config_name,"r")
-  if not f then return env, "Cannot open config file" end
-  local code = f:read("*all")
-  assert(run(code,env))
-  return env
+	local settings = settings or main_settings
+	env.settings = settings
+	local config_name = config_name or "config.lua"
+	local f = io.open(config_name,"r")
+	if not f then return env, "Cannot open config file" end
+	local code = f:read("*all")
+	assert(run(code,env))
+	return env
 end
 
 
