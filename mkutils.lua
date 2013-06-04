6ded9169359602aaa6c84e9d7f476a0eb55f095f
mr.mitch <mr.mitch@vfemail.net>
2013-06-04 20:28:58 +0200

    output dir working even with absolute paths

--- mkutils.lua
+++ mkutils.lua
@@ -67,62 +67,92 @@ function parse_lg(filename)
 	return {files = outputfiles, images = outputimages},status
 end
 
---function copy(src,dest, filter)
---  local src_f,dst_f=io.open(src,"r"),io.open(dest,"w")
---  local contents = src_f:read("*all")
---  local filter = filter or function(s) return s end
---  dst_f:write(filter(contents))
---  src_f:close()
---  dst_f:close()
---end
 
 local cp_func = os.type == "unix" and "cp" or "copy"
 function cp(src,dest)
-	local command = string.format("%s %s %s", cp_func, src, dest)
+	local command = string.format('%s %s %s', cp_func, src, dest)
 	if cp_func == "copy" then command = command:gsub("/",'\\') end
 	print("Copy: "..command)
 	os.execute(command)
 end
 
 local used_dir = {}
-function copy(filename,outfilename)
-	local parts = outfilename:split("/")
-	if type(parts) == "table" then
-		table.remove(parts,#parts)
-		local path = table.concat(parts,"/")
-		if not used_dir[path] then
-			print("Path to be created: "..path)
-			mkdirectories(parts)
-		end
-		used_dir[path]=true
-	end
-	if filename ~= outfilename then cp(filename,outfilename) end
-end
-mkdirectories = function(dirs)
-	local currdir = lfs.currentdir()
-	--print("Path: "..path) 
+
+function prepare_path(path)
 	--local dirs = path:split("/")
-	--table.remove(dirs,#dirs) 
-	for _,dir in ipairs(dirs) do
-		local status, msg = lfs.chdir(dir)
-		print("Chdir: ",dir,status,msg)
-		if not status then  
-			local status, msg = lfs.mkdir(dir)
-			print("Make: ",dir, status)
-			if not status then 
-				print("Path making error: "..msg)
-				break
-			else
-				lfs.chdir(dir)
-			end
-		end
-		--print(dir)
+	local dirs = {}
+	if path:match("^/") then dirs = {""}
+	elseif path:match("^~") then
+		local home = os.getenv "HOME"
+		dirs = home:split "/"
+		path = path:gsub("^~/","")
+		table.insert(dirs,1,"")
 	end
-	--lfs.copy(currdir.."/".."make.lua",currdir.."/"..path.."/make.lua")
-	lfs.chdir(currdir)
-	--os.execute("cp make.lua "..path)
+	for _,d in pairs(path:split "/") do
+		table.insert(dirs,d)
+	end
+	table.remove(dirs,#dirs)
+	return dirs,table.concat(dirs,"/")
 end
 
+-- Find which part of path already exists
+-- and which directories have to be created
+function find_directories(dirs, pos)
+	local pos = pos or #dirs
+	-- we tried whole path and no dir exist
+	if pos < 1 then return dirs end
+	local path = ""
+	-- in the case of unix absolute path, empty string is inserted in dirs
+	if pos == 1 and dirs[pos] == "" then
+		path = "/"
+	else
+		path = table.concat(dirs,"/", 1,pos) .. "/"
+	end
+	if not lfs.chdir(path)  then -- recursion until we succesfully changed dir
+	-- or there are no elements in the dir table
+	return find_directories(dirs,pos - 1)
+elseif pos ~= #dirs then -- if we succesfully changed dir
+	-- and we have dirs to create
+	local p = {}
+	for i = pos+1, #dirs do
+		table.insert(p, dirs[i])
+	end
+	return p
+else  -- whole path exists
+	return {}
+end
+end
+
+function mkdirectories(dirs)
+	if type(dirs) ~="table" then
+		return false, "mkdirectories: dirs is not table"
+	end
+	for _,d in pairs(dirs) do
+		local stat,msg = lfs.mkdir(d)
+		if not stat then return false, "makedirectories error: "..msg end
+		lfs.chdir(d)
+	end
+	return true
+end
+
+function copy(filename,outfilename)
+	local currdir = lfs.currentdir()
+	if filename == outfilename then return true end
+	local parts, path = prepare_path(outfilename)
+	if not used_dir[path] then 
+		local to_create, msg = find_directories(parts)
+		if not to_create then
+			print(msg)
+			return false
+		end
+		used_dir[path] = true
+		local stat, msg = mkdirectories(to_create)
+		if not stat then print(msg) end
+	end
+	lfs.chdir(currdir)
+	cp(filename, path)
+	return true
+end
 
 -- Config loading
 local function run(untrusted_code, env)
