6fc8ea531d86907ae0fb77832802c1c78c7fca1d
Michal Hoftich <michal.h21@gmail.com>
2013-06-18 17:47:55 +0200

    Added more parameters to optiontext

--- mkparams.lua
+++ mkparams.lua
@@ -5,19 +5,22 @@ local m = {} -- use ugly module system for new lua versions support
 local function get_args(parameters, optiontext)
 	local parameters = parameters or {}
 	parameters.progname = parameters.progname or "make4ht"
+	parameters.postparams = parameters.postparams or ""
+	parameters.postfile = parameters.postfile or ""
 	local optiontext = optiontext or  [[
 ${progname} - build system for tex4ht
 Usage:
-make4ht [options] filename ["tex4ht.sty op." "tex4ht op." "t4ht op" "latex op"]
-  -c,--config (default xhtml) Custom config file
-	-d,--output-dir (default nil)  Output directory
-	-l,--lua  Use lualatex for document compilation
-	-s,--shell-escape Enables running external programs from LaTeX
-	-u,--utf8  For output documents in utf8 encoding
-	-x,--xetex Use xelatex for document compilation
-	<filename> (string) Input file name
+${progname} [options] filename ["tex4ht.sty op." "tex4ht op." "t4ht op" "latex op"]
+-c,--config (default xhtml) Custom config file
+-d,--output-dir (default nil)  Output directory
+-l,--lua  Use lualatex for document compilation
+-s,--shell-escape Enables running external programs from LaTeX
+-u,--utf8  For output documents in utf8 encoding
+-x,--xetex Use xelatex for document compilation
 ]]
-  return lapp(optiontext % parameters)
+	optiontext = optiontext .. parameters.postparams .."<filename> (string) Input file name\n" .. parameters.postfile 
+	print("--------------\n" .. optiontext .."--------------\n")
+	return lapp(optiontext % parameters)
 end
 m.get_args = get_args
 return m
