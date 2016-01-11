140198b9af27342d3d156bc74e716fc0b1dd33e0
Michal Hoftich <michal.h21@gmail.com>
2016-01-11 09:18:05 +0100

    Fixed bug in loading documents specified with full path

--- mkparams.lua
+++ mkparams.lua
@@ -74,6 +74,8 @@ local function process_args(args)
 	insert_latex("shell-escape","-shell-escape")
   local latex_cli_params = args[4] or ""
   if not latex_cli_params:match("%-jobname") then
+    -- we must strip out directories from jobname when full path to document is given
+    input = input:match("([^%/]+)$")
     table.insert(latex_params,"-jobname="..input)
   else
     -- when user specifies -jobname, we must change name of the input file,
