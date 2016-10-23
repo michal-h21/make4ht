db792387d46389c1346be119adb79841341487ed
Michal Hoftich <michal.h21@gmail.com>
2016-10-23 23:26:20 +0200

    Added support for local fonts

--- mathnode.lua
+++ mathnode.lua
@@ -6,6 +6,13 @@ local mkutils = require "mkutils"
 local mathnodepath = "page2html"
 -- options for MathJax command
 local options = "--format MathML"
+-- math fonts position
+-- don't alter fonts if not set
+local fontdir = nil
+-- if we copy fonts 
+local fontdest = nil
+local fontformat = "otf"
+
 local function compile(src)
   local tmpfile = os.tmpname()
   local filename = src
@@ -35,6 +42,26 @@ local function extract_css(file)
   return filename, css
 end
 
+-- 
+local function use_fonts(css)
+  local family_pattern = "font%-family:%s*(.-);.-%/([^%/]+)%.".. fontformat
+  local family_build = "@font-face {font-family: %s; src: url('%s/%s.%s') format('%s')}"
+  local fontdir = fontdir:gsub("/$","")
+  css = css:gsub("(@font%-face%s*{.-})", function(face)
+    -- if not face:match("url%(") then return face end
+    if not face:match("url%(") then return "" end
+    -- print(face)
+    local family, filename = face:match(family_pattern)
+    print(family, filename)
+    local newfile = string.format("%s/%s.%s", fontdir, filename, fontformat)
+    Make:add_file(newfile)
+    return family_build:format(family, fontdir, filename, fontformat, fontformat)
+    -- return face
+  end)
+  return css
+end
+
+
 local function save_css(filename, css)
   local f = io.open(filename, "w")
   f:write(css)
@@ -45,9 +72,16 @@ return function(text, arguments)
   -- if arguments.prg then mathnodepath = arguments.prg end
   mathnodepath = arguments.prg or mathnodepath
   options      = arguments.options or options
+  fontdir      = arguments.fontdir or fontdir
+  fontdest     = arguments.fontdest or fontdest
+  fontformat   = arguments.fontformat or fontformat
   compile(text)
   filename, css = extract_css(text)
+  if fontdir then
+    css = use_fonts(css)
+  end
   save_css(filename, css)
+  Make:add_file(filename)
   -- print(css)
   print(filename)
 end
