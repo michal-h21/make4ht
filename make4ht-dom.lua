525633aeb8cae92bb6e3d8afb330635c3e808e3c
Michal Hoftich <michal.h21@gmail.com>
2016-11-07 15:48:00 +0100

    Basic dom parser and serializer

---
+++ make4ht-dom.lua
@@ -0,0 +1,131 @@
+local sentenceparser = {}
+local xml = require("luaxml-mod-xml")
+local handler = require("luaxml-mod-handler")
+
+
+local void = {area = true, base = true, br = true, col = true, hr = true, img = true, input = true, link = true, meta = true, param = true}
+
+local actions = {
+  TEXT = {text = "%s"},
+  COMMENT = {start = "<!-- ", text = "%s", stop = " -->"},
+  ELEMENT = {start = "<%s%s>", stop = "</%s>", void = "<%s%s />"},
+  DECL = {start = "<?%s %s?>"},
+  DTD = {start = "<!DOCTYPE ", text = "%s" , stop=">"}
+}
+
+local function serialize_dom(parser, current,level, output)
+  local output = output or {}
+  local function get_action(typ, action)
+    local ac = actions[typ] or {}
+    local format = ac[action] or ""
+    return format
+  end
+  local function insert(format, ...)
+    table.insert(output, string.format(format, ...))
+  end
+  local function prepare_attributes(attr)
+    local t = {}
+    local attr = attr or {}
+    for k, v in pairs(attr) do
+      t[#t+1] = string.format("%s='%s'", k, v)
+    end
+    if #t == 0 then return "" end
+    -- add space before attributes
+    return " " .. table.concat(t, " ")
+  end
+  local function start(typ, el, attr)
+    local format = get_action(typ, "start")
+    insert(format, el, prepare_attributes(attr))
+  end
+  local function text(typ, text)
+    local format = get_action(typ, "text")
+    insert(format, text)
+  end
+  local function stop(typ, el)
+    local format = get_action(typ, "stop")
+    insert(format,el)
+  end
+  local level = level or 0
+  local spaces = string.rep(" ",level)
+  local root= current or parser._handler.root
+  local name = root._name or "unnamed"
+  local xtype = root._type or "untyped"
+  local text_content = root._text or ""
+  local attributes = root._attr or {}
+  -- if xtype == "TEXT" then
+  --   print(spaces .."TEXT : " .. root._text)
+  -- elseif xtype == "COMMENT" then
+  --   print(spaces .. "Comment : ".. root._text)
+  -- else
+  --   print(spaces .. xtype .. " : " .. name)
+  -- end
+  -- for k, v in pairs(attributes) do
+  --   print(spaces .. " ".. k.."="..v)
+  -- end
+  if xtype == "DTD" then
+    text_content = string.format('%s %s "%s" "%s"', name, attributes["_type"],  attributes._name, attributes._uri )
+    attributes = {}
+  elseif xtype == "ELEMENT" and void[name] then
+    local format = get_action(xtype, "void")
+    insert(format, name, prepare_attributes(attributes))
+    return output
+  end
+
+  start(xtype, name, attributes)
+  text(xtype,text_content) 
+  local children = root._children or {}
+  for _, child in ipairs(children) do
+    output = serialize_dom(parser,child, level + 1, output)
+  end
+  stop(xtype, name)
+  return output
+end
+
+local parse = function(x)
+  local domHandler = handler.domHandler()
+  local parser = xml.xmlParser(domHandler)
+  -- preserve whitespace
+  parser.options.stripWS = nil
+  parser:parse(x)
+  parser.current = parser._handler.root
+  function parser.get_path(self,path, current)
+    local function traverse_path(path_elements, current, t)
+      local t = t or {}
+      if #path_elements == 0 then 
+        for _, x in ipairs(current or {}) do
+          print "mame match"
+          table.insert(t,x)
+        end
+        return t
+      end
+      local current_path = table.remove(path_elements, 1)
+      print("current path", current_path)
+      for _, x in ipairs(current._children or {}) do
+        if x._type == "ELEMENT" then
+          local name = string.lower(x._name)
+          print(name, current_path) 
+          if name == current_path then
+            for _, child in ipairs(x._children) do
+              t = traverse_path(path_elements, child, t)
+            end
+          end
+        end
+      end
+      return t
+    end
+    local current = current or self._handler.root
+    local path_elements = {}
+    local path = string.lower(path)
+    for el in path:gmatch("([^%s]+)") do table.insert(path_elements, el) end
+    return traverse_path(path_elements, current)
+  end
+    
+  -- parser:
+  return parser
+end
+
+
+local M = {}
+M.parse = parse
+M.serialize= serialize_dom
+return M
