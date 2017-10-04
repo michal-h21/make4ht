1f88ca44c942fd9112ca44a64f41bb2dd01fc55c
Michal Hoftich <michal.h21@gmail.com>
2017-10-04 17:57:22 +0200

    basic formats work

--- formats/html5.lua
+++ formats/html5.lua
@@ -3,6 +3,7 @@ local M = {}
 local mkutils = require "mkutils"
 
 function M.prepare_parameters(parameters,extensions)
+  parameters.tex4ht_sty_par  = parameters.tex4ht_sty_par .. ",html5"
   parameters = mkutils.extensions_prepare_parameters(extensions,parameters)
   return parameters
 end
