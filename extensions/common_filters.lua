4cb6611d89864edebb56817fe27b419cd38d45e7
Michal Hoftich <michal.h21@gmail.com>
2017-10-06 14:55:10 +0200

    fix: filters should be first matcher to be executed

--- extensions/common_filters.lua
+++ extensions/common_filters.lua
@@ -4,9 +4,22 @@ local M = {}
 local filter = require "make4ht-filter"
 local process = filter {"cleanspan-nat", "fixligatures", "hruletohr", "entities", "fix-links"}
 
+-- filters support only html formats
+function M.test(format)
+  if format == "odt" then return false end
+  return true
+end
 
 function M.modify_build(make)
   make:match("html$", process)
+  local matches = make.matches
+  -- the filters should be first match to be executed, especially if tidy
+  -- should be executed as well
+  if #matches > 1 then
+    local last = matches[#matches]
+    table.insert(matches, 1, last)
+    matches[#matches] = nil
+  end
   return make
 end
 
