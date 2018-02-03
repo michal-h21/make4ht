075d307317c51d94ea557d92c44a7d6e45c98777
Michal Hoftich <michal.h21@gmail.com>
2018-02-03 21:38:49 +0100

    deleted spurious line

--- formats/odt.lua
+++ formats/odt.lua
@@ -2,7 +2,6 @@ local M = {}
 local mkutils = require "mkutils"
 
 function M.prepare_parameters(settings, extensions)
-
   settings.tex4ht_sty_par = settings.tex4ht_sty_par ..",ooffice"
   settings.tex4ht_par = settings.tex4ht_par .. " ooffice/! -cmozhtf"
   settings.t4ht_par = settings.t4ht_par .. " -cooxtpipes -coo "
