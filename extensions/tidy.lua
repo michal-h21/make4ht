34bfc90b3c0102cbc615331097f4625d209ebd29
Michal Hoftich <michal.h21@gmail.com>
2018-02-11 21:17:26 +0100

    Fixed support for LuaXML

--- extensions/tidy.lua
+++ extensions/tidy.lua
@@ -5,13 +5,52 @@ function M.test(format)
   return true
 end
 
+local empty_elements = {
+  area=true,
+  base=true,
+  br=true,
+  col=true,
+  embed=true,
+  hr=true,
+  img=true,
+  input=true,
+  keygen=true,
+  link=true,
+  meta=true,
+  param=true,
+  source=true,
+  track=true,
+  wbr=true,
+}
+
+-- LuaXML cannot read HTML with unclosed tags (like <meta name="hello" content="world">)
+-- Tidy removes end slashes in the HTML output, so
+-- this function will add them back
+local function close_tags(s)
+  return s:gsub("<(%w+)([^>]-)>", function(tag, rest)
+    local endslash = ""
+    if empty_elements[tag] then endslash = " /" end
+    return string.format("<%s%s%s>", tag, rest, endslash)
+  end)
+end
+    
+
+
 function M.modify_build(make)
   make:match("html$", function(filename, par)
     local settings = get_filter_settings "tidy" or {}
-    par.options = par.options or settings.options or "-m -utf8 -w 512 -q"
+    par.options = par.options or settings.options or "-utf8 -w 512 -ashtml -q"
     local command = "tidy ${options}  ${filename}" % par
     print("execute: ".. command)
-    os.execute(command)
+    -- os.execute(command)
+    local run = io.popen(command, "r")
+    local result = run:read("*all")
+    run:close()
+    result = close_tags(result)
+    print(result)
+    local f = io.open(filename, "w")
+    f:write(result)
+    f:close()
   end)
   return make
 end
