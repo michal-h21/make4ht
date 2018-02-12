66691c18caa3f3808904d63d88fcb4c8be1a3f01
Michal Hoftich <michal.h21@gmail.com>
2018-02-12 15:40:18 +0100

    Template for extension loading

--- formats/html5.lua
+++ formats/html5.lua
@@ -2,6 +2,11 @@ local M = {}
 
 local mkutils = require "mkutils"
 
+function M.prepare_extensions(extensions)
+  -- return mkutils.add_extensions("+tidy", extensions)
+  return extensions --mkutils.add_extensions("+tidy", extensions)
+end
+
 function M.prepare_parameters(parameters,extensions)
   parameters.tex4ht_sty_par  = parameters.tex4ht_sty_par .. ",html5"
   parameters = mkutils.extensions_prepare_parameters(extensions,parameters)
