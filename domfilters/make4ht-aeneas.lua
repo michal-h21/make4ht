ec7aa5c4164158c30126ce4583cd375bd0deadc6
Michal Hoftich <michal.h21@gmail.com>
2018-02-21 15:34:08 +0100

    Aeneas filter done

--- domfilters/make4ht-aeneas.lua
+++ domfilters/make4ht-aeneas.lua
@@ -22,26 +22,33 @@ local function prepare_selectors(skips)
   return css
 end
 
+-- make span element with unique id for a sentence
 local function make_span(id,parent, text)
-  local newobj = parent:create_element("span", {id=id, class=id})
-  newobj.processed = true
+  local newobj = parent:create_element("span", {id=id }) 
+  newobj.processed = true -- to disable multiple processing of the node
   local text_node = newobj:create_text_node(text)
   newobj:add_child_node(text_node)
   return newobj
 end
 
+-- make the id attribute and update the id value
 local function make_id(lastid, id_prefix)
   local id = id_prefix .. lastid
   lastid = lastid + 1
   return id, lastid
 end
 
+-- parse text for sentences and add spans 
 local function make_ids(parent, text, lastid, id_prefix)
   local t = {}
+  local id
   for chunk, punct in text:gmatch("([^%.^%?^!]*)([%.%?!]?)") do
     id, lastid = make_id(lastid, id_prefix)
-    print(id, chunk, punct)
-    table.insert(t, make_span(id, parent, chunk .. punct))
+    local newtext = chunk..punct
+    -- the newtext is empty string sometimes. we can skipt it then.
+    if newtext~="" then
+      table.insert(t, make_span(id, parent, newtext))
+    end
   end
   return t, lastid
 end
@@ -61,20 +68,32 @@ local function aeneas(dom, par)
   local id_prefix = options.id_prefix or par.id_prefix or id_prefix
   local skip_object = prepare_selectors(skip_elements)
   local body = dom:query_selector("body")[1]
+  -- process only the document body
   if not body then return dom end
   body:traverse_elements(function(el)
     -- skip disabled elements
     if(is_skipped(el, skip_object)) then return false end
-    -- skip already processed elements 
+    -- skip already processed elements
     if el.processed then return false end
-    local newchildren = {}
-    if #el:get_children() == 1 and el._children[1]:is_text() and not el:get_attribute("id") then
+    local newchildren = {} -- this will contain the new elements
+    local children = el:get_children()
+    local first_child = children[1]
+
+    -- if the element contains only text, doesn't already have an id attribute and the text is short,
+    -- the id is set directly on that element.
+    if #children == 1
+      and first_child:is_text()
+      and not el:get_attribute("id")
+      and string.len(first_child._text) < 20
+    then
       local idtitle
       idtitle, id = make_id(id, id_prefix)
       el:set_attribute("id", idtitle)
       return el
     end
-    for _, child in ipairs(el:get_children()) do
+
+    for _, child in ipairs(children) do
+      -- process only non-empty text
       if child:is_text() and child._text:match("%a+") then
         local newnodes
         newnodes, id = make_ids(child, child._text, id, id_prefix)
@@ -82,23 +101,19 @@ local function aeneas(dom, par)
           table.insert(newchildren, node or {})
         end
       else
+        -- insert the current processing element to the new element list
+        -- if it isn't only text
         table.insert(newchildren, child or {})
       end
     end
-    for k,v in ipairs(newchildren) do
-      print(k, v:get_node_type(),v._name)
-    end
+    -- replace element children with the new ones
     if #newchildren > 0 then
-      el._children = {} -- newchildren
-      -- local newel = el:copy_node()
-      -- newel._children = {}
+      el._children = {}
       for _, c in ipairs(newchildren) do
         el:add_child_node(c)
       end
-      -- el:replace_node(newel)
     end
   end)
-  print "*************************"
   return dom
 end
 
