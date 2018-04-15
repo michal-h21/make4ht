c6f816f06a4e725879ea58251202f2ac912a996c
Michal Hoftich <michal.h21@gmail.com>
2018-04-15 16:17:42 +0200

    Extracted the yaml header filter

--- extensions/staticsite.lua
+++ extensions/staticsite.lua
@@ -1,6 +1,5 @@
 local M = {}
 local filter = require "make4ht-filter"
-local domobj = require "luaxml-domobject"
 local mkutils = require "mkutils"
 
 -- get the published file name
@@ -27,56 +26,6 @@ local function get_slug(settings)
 end
 
 
--- save the header settings in YAML format
-local function make_yaml(tbl, level)
-  local t = {}
-  local level = level or 0
-  local indent = string.rep("  ", level)
-  -- indentation for multilen strings
-  local str_indent = string.rep("  ", level + 1)
-  for k,v in pairs(tbl) do
-    if type(v)=="string" then
-      -- detect multiline strings
-      if v:match("\n") then
-        table.insert(t, string.format(indent .. "%s: |", k))
-        v = str_indent .. (v:gsub("\n", "\n".. str_indent))
-      else
-        v = v:gsub("'", "''")
-        table.insert(t, string.format(indent .. "%s: '%s'", k,v))
-      end
-    elseif type(v) == "table" then
-      table.insert(t,string.format(indent .. "%s:", k))
-      -- we need to differently process array and hash table
-      -- we don't support mixing types
-      if #v > 0 then
-        for x,y in ipairs(v) do
-          if type(y) == "string" then
-            -- each string can be printed on it's own line
-            table.insert(t, indent .. string.format("- '%s'", y))
-          else
-            -- subtables need to be indented
-            table.insert(t, indent .. "-")
-            table.insert(t, make_yaml(y, level + 1))
-          end
-        end
-      else
-        -- print indented table
-        table.insert(t, make_yaml(v,level + 1))
-      end
-    else
-      -- convert numbers and other values to string
-      table.insert(t, string.format(indent .. "%s: %s", k,tostring(v)))
-    end
-    
-  end
-  return table.concat(t,  "\n")
-end
-
-local function get_header(tbl)
-  local yaml = make_yaml(tbl)
-  return "---\n".. yaml.. "\n---\n"
-end
-
 local function update_jobname(slug, latex_par)
   local latex_par = latex_par or ""
   if latex_par:match("%-jobname") then
@@ -93,35 +42,28 @@ local function update_jobname(slug, latex_par)
 end
 
 function M.modify_build(make)
+  local function insert_filter(outdir)
+    table.insert(make.matches, 1, {
+      pattern=".*",
+      params = {}, 
+      command = function()
+        for _, match in ipairs(make.matches) do
+          match.params.outdir = outdir
+          print(match.pattern, match.params.outdir)
+        end
+        -- os.exit()
+      end
+    })
+
+    -- local first = make.matches[1]
+    -- for k,v in pairs(first) do
+    --   print("xxx",k,v)
+    -- end
+    -- os.exit()
+  end
   -- I should make filter from this
   local process = filter {
-    function(s,par)
-      print(os.getenv "blog_home")
-      local dom = domobj.parse(s)
-      local properties = {}
-      local head = dom:query_selector("head")[1]
-      properties.title = head:query_selector("title")[1]:get_text()
-      local styles = {}
-      for _, link in ipairs(head:query_selector("link")) do
-        local typ = link:get_attribute("type")
-        if typ == "text/css" then 
-          table.insert(styles, link:get_attribute("href"))
-        end
-      end
-      properties.styles = styles
-      local metas = {}
-      for _, meta in ipairs(head:query_selector("meta")) do
-        print(meta:serialize())
-        table.insert(metas, {charset= meta:get_attribute("charset"), content = meta:get_attribute("content"), property = meta:get_attribute("property"), name = meta:get_attribute("name")})
-      end
-      properties.meta = metas
-
-
-      local body = dom:query_selector("body")[1]
-      print(get_header(properties))
-      -- return s
-      return get_header(properties) .. body:serialize():gsub("<body.->", ""):gsub("</body>", "")
-    end
+    "staticsite"
   }
   local settings = make.params
   -- get the published file name
@@ -136,10 +78,8 @@ function M.modify_build(make)
   local site_settings = get_filter_settings "staticsite"
   local outdir = site_settings.output_dir
 
-  if outdir then
-    settings.outdir = outdir
-    set_settings {outdir=outdir}
-  end
+  insert_filter(outdir)
+
   local quotepattern = '(['..("%^$().[]*+-?"):gsub("(.)", "%%%1")..'])'
   local mainfile = string.gsub(slug, quotepattern, "%%%1")
   -- is this useful for anything?
---
+++ filters/make4ht-staticsite.lua
@@ -0,0 +1,78 @@
+local domobj = require "luaxml-domobject"
+-- save the header settings in YAML format
+local function make_yaml(tbl, level)
+  local t = {}
+  local level = level or 0
+  local indent = string.rep("  ", level)
+  -- indentation for multilen strings
+  local str_indent = string.rep("  ", level + 1)
+  for k,v in pairs(tbl) do
+    if type(v)=="string" then
+      -- detect multiline strings
+      if v:match("\n") then
+        table.insert(t, string.format(indent .. "%s: |", k))
+        v = str_indent .. (v:gsub("\n", "\n".. str_indent))
+      else
+        v = v:gsub("'", "''")
+        table.insert(t, string.format(indent .. "%s: '%s'", k,v))
+      end
+    elseif type(v) == "table" then
+      table.insert(t,string.format(indent .. "%s:", k))
+      -- we need to differently process array and hash table
+      -- we don't support mixing types
+      if #v > 0 then
+        for x,y in ipairs(v) do
+          if type(y) == "string" then
+            -- each string can be printed on it's own line
+            table.insert(t, indent .. string.format("- '%s'", y))
+          else
+            -- subtables need to be indented
+            table.insert(t, indent .. "-")
+            table.insert(t, make_yaml(y, level + 1))
+          end
+        end
+      else
+        -- print indented table
+        table.insert(t, make_yaml(v,level + 1))
+      end
+    else
+      -- convert numbers and other values to string
+      table.insert(t, string.format(indent .. "%s: %s", k,tostring(v)))
+    end
+    
+  end
+  return table.concat(t,  "\n")
+end
+
+local function get_header(tbl)
+  local yaml = make_yaml(tbl)
+  return "---\n".. yaml.. "\n---\n"
+end
+
+return function(s,par)
+  print(os.getenv "blog_home")
+  local dom = domobj.parse(s)
+  local properties = {}
+  local head = dom:query_selector("head")[1]
+  properties.title = head:query_selector("title")[1]:get_text()
+  local styles = {}
+  for _, link in ipairs(head:query_selector("link")) do
+    local typ = link:get_attribute("type")
+    if typ == "text/css" then 
+      table.insert(styles, link:get_attribute("href"))
+    end
+  end
+  properties.styles = styles
+  local metas = {}
+  for _, meta in ipairs(head:query_selector("meta")) do
+    print(meta:serialize())
+    table.insert(metas, {charset= meta:get_attribute("charset"), content = meta:get_attribute("content"), property = meta:get_attribute("property"), name = meta:get_attribute("name")})
+  end
+  properties.meta = metas
+
+
+  local body = dom:query_selector("body")[1]
+  print(get_header(properties))
+  -- return s
+  return get_header(properties) .. body:serialize():gsub("<body.->", ""):gsub("</body>", "")
+end
