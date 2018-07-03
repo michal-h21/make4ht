230b77d939b4ebda27dfb804e946158d978bf2c3
Michal Hoftich <michal.h21@gmail.com>
2018-07-03 20:39:24 +0200

    Don't convert amperesand and <> entities to characters

--- filters/make4ht-entities-to-unicode.lua
+++ filters/make4ht-entities-to-unicode.lua
@@ -1,9 +1,13 @@
 -- convert Unicode characters encoded as XML entities back to Unicode
 
+-- list of disabled characters
+local disabled = { ["&"] = "&amp;", ["<"] = "&lt;", [">"] = "&gt;" }
 local utfchar = unicode.utf8.char
 return  function(content)
   return content:gsub("%&%#x([A-Fa-f0-9]+);", function(entity)
     -- convert hexadecimal entity to Unicode
-    return utfchar(tonumber(entity, 16))
+    local newchar =  utfchar(tonumber(entity, 16))
+    -- we don't want to break XML validity with forbidden characters
+    return disabled[newchar] or newchar
   end)
 end
