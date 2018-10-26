a1ecc49550afc0271ad03aae0591d4f384716e77
Michal Hoftich <michal.h21@gmail.com>
2018-10-26 23:40:24 +0200

    convert non-breaking space character to entity

--- filters/make4ht-entities-to-unicode.lua
+++ filters/make4ht-entities-to-unicode.lua
@@ -1,13 +1,15 @@
 -- convert Unicode characters encoded as XML entities back to Unicode
 
--- list of disabled characters
-local disabled = { ["&"] = "&amp;", ["<"] = "&lt;", [">"] = "&gt;" }
 local utfchar = unicode.utf8.char
+-- list of disabled characters
+local disabled = { ["&"] = "&amp;", ["<"] = "&lt;", [">"] = "&gt;"}
 return  function(content)
-  return content:gsub("%&%#x([A-Fa-f0-9]+);", function(entity)
+   local content = content:gsub("%&%#x([A-Fa-f0-9]+);", function(entity)
     -- convert hexadecimal entity to Unicode
     local newchar =  utfchar(tonumber(entity, 16))
     -- we don't want to break XML validity with forbidden characters
     return disabled[newchar] or newchar
   end)
+  -- the non-breaking space character cause issues in the ODT opening
+  return content:gsub(string.char(160), "&#xA0;")
 end
