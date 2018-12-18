b0f2235cc68c3d4ba89dcac5fcaa70a5924aab11
Michal Hoftich <michal.h21@gmail.com>
2018-12-18 15:30:47 +0100

    Fixed issue in the parameters table passing

--- filters/make4ht-filter.lua
+++ filters/make4ht-filter.lua
@@ -4,9 +4,9 @@ local function load_filter(filtername)
 	return require("filters.make4ht-"..filtername)
 end
 
-function filter(filters,parameters)
+function filter(filters)
   local sequence = filter_lib.load_filters(filters, load_filter)
-	return function(filename)
+	return function(filename, parameters)
 		if not filename then return false, "filters: no filename" end
     local input = filter_lib.load_input_file(filename)
     if not input  then return nil, "Cannot load the input file" end
