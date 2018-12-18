522001704d1f156bd0a920bf8981516a7ba0c1ac
Michal Hoftich <michal.h21@gmail.com>
2018-12-18 15:55:07 +0100

    Various fixes

--- filters/make4ht-odttemplate.lua
+++ filters/make4ht-odttemplate.lua
@@ -1,10 +1,6 @@
-local filter = require "make4ht-filter"
 local mkutils = require "mkutils"
 local zip = require "zip"
 
--- filter_settings "odttemplate" {
---   filename = "test.odt"
--- }
 
 local function get_template_filename(settings)
   -- either get the template odt filename from tex4ht.sty options (make4ht filename.tex "odttemplate=test.odt")
@@ -13,19 +9,22 @@ local function get_template_filename(settings)
   if templatefile then return templatefile end
   -- read the template odt filename from settings
   local filtersettings = get_filter_settings "odttemplate"
-  return filtersettings.filename
+  return settings.template or filtersettings.template
 end
 
 return function(content, settings)
   -- use settings added from the Make:match, or default settings saved in Make object
-  local settings = settings or Make.params
   local templatefile = get_template_filename(settings)
   -- don't do anything if the template file doesn't exist
   if not templatefile or not mkutils.file_exists(templatefile) then return content end
   local odtfile = zip.open(templatefile)
-  local stylesfile = odtfile:open("styles.xml")
-  -- just break if the styles cannot be found
-  if not stylesfile then return content end
-  local styles = stylesfile:read("*all")
-  return styles
+  if odtfile then
+    local stylesfile = odtfile:open("styles.xml")
+    -- just break if the styles cannot be found
+    if not stylesfile then return content end
+    local styles = stylesfile:read("*all")
+    return styles
+  end
+  -- just return content in the case of problems
+  return content
 end
