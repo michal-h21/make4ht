8be50c3f1ec0cd3572b410aeef9ac785fb4934c8
Michal Hoftich <michal.h21@gmail.com>
2019-09-13 14:04:29 +0200

    Added support for Rmd, Rrst, Rtex, md and rst

--- extensions/preprocess_input.lua
+++ extensions/preprocess_input.lua
@@ -2,22 +2,36 @@
 local M = {}
 
 local commands = {
-  knitr = { command = 'Rscript -e "library(knitr); knit(\'${tex_file}\', output=\'${tmp_file}\')"'}
+  knitr = { command = 'Rscript -e "library(knitr); knit(\'${tex_file}\', output=\'${tmp_file}\')"'},
+  pandoc = { command = 'pandoc -f ${input_format} -s -o \'${tmp_file}\' -t latex \'${tex_file}\''}
 }
 local filetypes = {
   rnw = {sequence = {"knitr"} },
-
+  rtex = {sequence = {"knitr"}},
+  rmd = {sequence = {"knitr", "pandoc"}, options = {input_format = "markdown"}},
+  rrst = {sequence = {"knitr", "pandoc"}, options = {input_format = "rst"}},
+  md = {sequence = {"pandoc"}, options = {input_format = "markdown"}},
+  rst = {sequence = {"pandoc"}, options = {input_format = "rst"}},
 }
 
 
 local function execute_sequence(sequence, arg, make)
   -- keep track of all generated tmp files
   local temp_files = {}
+  -- the temporary file for the current compilation step 
+  -- should become the tex_file for the next one. It doesn't
+  -- matter that it isn't TeX file in some cases
+  local previous_temp 
   for _, cmd_name in ipairs(sequence) do
     local tmp_name = os.tmpname()
     temp_files[#temp_files+1] = tmp_name
     -- make the temp file name accessible to the executed commands
     arg.tmp_file = tmp_name
+    -- the current temporary file should become tex_file in the next step
+    -- in the first execution of the compilation sequence we will use the 
+    -- actual input file name
+    arg.tex_file = previous_temp or arg.tex_file
+    previous_temp = tmp_name
     -- get the command to execute
     local cmd  = commands[cmd_name]
     -- fill the command template with make4ht arguments and execute
@@ -41,6 +55,14 @@ local function get_preprocessing_pipeline(input_file)
     return matched
 end
 
+-- join the make4ht params and command options tables
+local function make_options(arg, command_options)
+  local options = {}
+  local command_options = command_options or {}
+  for k,v in pairs(arg) do options[k] = v end
+  for k,v in pairs(command_options) do options[k] = v end
+  return options
+end
 
 M.modify_build = function(make)
 
@@ -52,13 +74,16 @@ M.modify_build = function(make)
     print("preprocess_input error: ".. msg)
     return
   end
+  -- prepare options 
+  local options = make_options(arg, matched.options)
   -- run the execution sequence
-  local temp_files = execute_sequence(matched.sequence or {}, arg, make)
+  local temp_files = execute_sequence(matched.sequence or {}, options, make)
   -- the last temporary file contains the actual TeX file
   local last_temp_file = temp_files[#temp_files]
   -- remove the intermediate temp files
   if #temp_files > 2 then
     for i = 1, #temp_files - 1 do
+      print("Removing temporary file", temp_files[i])
       os.remove(temp_files[i])
     end
   end
