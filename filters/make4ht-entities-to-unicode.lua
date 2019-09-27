ead41242d62d9e5e81cb636b6043b7c2bc61f5c0
Michal Hoftich <michal.h21@gmail.com>
2019-09-27 16:09:01 +0200

    Really fixed the Unicode entities handling

--- filters/make4ht-entities-to-unicode.lua
+++ filters/make4ht-entities-to-unicode.lua
@@ -7,8 +7,8 @@ return  function(content)
    local content = content:gsub("%&%#x([A-Fa-f0-9]+);", function(entity)
     -- convert hexadecimal entity to Unicode
     local char_number = tonumber(entity, 16)
-    -- don't convert entites that would produce invalid UTF-8 chars
-    if char_number > 127 and char_number < 256 then return "&#x".. entity ..";" end
+    -- fix for non-breaking spaces, LO cannot open file when they are present as Unicode
+    if char_number == 160 then return  "&#xA0;" end
     local newchar =  utfchar(char_number)
     -- we don't want to break XML validity with forbidden characters
     return disabled[newchar] or newchar
