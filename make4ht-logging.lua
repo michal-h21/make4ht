8b1116d0780ffb2b1a6a2b67ca675875376fc4fd
Michal Hoftich <michal.h21@gmail.com>
2019-10-23 13:08:08 +0200

    use tostring in the table prepared for table.concat

--- make4ht-logging.lua
+++ make4ht-logging.lua
@@ -71,7 +71,7 @@ function logging.new(module)
         -- trick used to support the nil values in the varargs
         -- https://stackoverflow.com/a/7186820/2467963
         for i= 1, table_with_holes.n do
-          table.insert(table_without_holes, table_with_holes[i] or "")
+          table.insert(table_without_holes, tostring(table_with_holes[i]) or "")
         end
         local msg = table.concat(table_without_holes, "\t")
         logging.print_msg(string.upper(name),  string.format("%s: %s", self.module, msg), color)
