ec6be7d63039750e6bdb1923e175d11e2b1ef00f
Michal Hoftich <michal.h21@gmail.com>
2019-12-30 19:34:43 +0100

    Fixed <mi> element handling

--- domfilters/make4ht-joincharacters.lua
+++ domfilters/make4ht-joincharacters.lua
@@ -3,28 +3,24 @@ local log = logging.new("joincharacters")
 local charclasses = {
   span=true,
   mn = true,
-  mi = true
 }
 
-local function update_mathvariant(el, next_el)
-  local set_mathvariant = function(curr)
-    -- when we join several <mi> elements, they will be rendered incorrectly
-    -- we must set the mathvariant attribute
-    local parent = curr:get_parent()
-    -- set mathvariant only if it haven't been set by the parent element
-    if not parent:get_attribute("mathvariant") then
-      curr._attr = curr._attr or {}
-      local mathvariant = curr:get_attribute("mathvariant") or "italic"
-      curr:set_attribute("mathvariant", mathvariant)
-    end
-  end
-  if el:get_attribute("mathvariant") == next_el:get_attribute("mathvariant") then
-    set_mathvariant(el)
-    set_mathvariant(next_el)
+local function update_mathvariant(curr)
+  -- when we join several <mi> elements, they will be rendered incorrectly
+  -- we must set the mathvariant attribute
+  local parent = curr:get_parent()
+  -- set mathvariant only if it haven't been set by the parent element
+  if not parent:get_attribute("mathvariant") then
+    -- curr._attr = curr._attr or {}
+    local mathvariant = "italic"
+    -- the joined elements don't have attributes
+    curr._attr = curr._attr or {}
+    curr:set_attribute("mathvariant", mathvariant)
   end
 end
 
 local table_count = function(tbl)
+  local tbl = tbl or {}
   local i = 0
   for k,v in pairs(tbl) do i = i + 1 end
   return i
@@ -40,10 +36,6 @@ local has_matching_attributes = function (el, next_el)
     -- if any attribute doesn't match, elements don't match
     if v~=next_attr[k] then return false end
   end
-  -- fix <mi> elements mathvariant attributes
-  if el:get_element_name() == "mi" then 
-    update_mathvariant(el, next_el) 
-  end
   return true
 end
 
@@ -55,45 +47,48 @@ local function join_characters(obj,par)
   local options = get_filter_settings "joincharacters"
   local charclasses = options.charclasses or par.charclasses or charclasses
 
+  local get_name = function(curr) 
+    return string.lower(curr:get_element_name())
+  end
+  local get_class = function(next_el)
+    return next_el:get_attribute("class") or next_el:get_attribute("mathvariant")
+  end
+  local is_span = function(next_el)
+    return charclasses[get_name(next_el)]
+  end
+  local join_elements = function(el, next_el)
+    -- it the following element match, copy it's children to the current element
+    for _, child in ipairs(next_el:get_children()) do
+      el:add_child_node(child)
+    end
+    -- remove the next element
+    next_el:remove_node()
+  end
+
+  local function get_next(curr, class)
+    local next_el = curr:get_next_node()
+    if next_el and next_el:is_element() and is_span(next_el) then
+      return next_el
+      -- if the next node is space followed by a matching element, we should add this space
+    elseif next_el and next_el:is_text() and get_next(next_el, class) then
+      local text = next_el._text 
+      -- match only text containing just whitespace
+      if text:match("^%s+$") then return next_el end
+    end
+  end
 
   obj:traverse_elements(function(el)
-    local get_name = function(curr) 
-      return string.lower(curr:get_element_name())
-    end
-    local get_class = function(next_el)
-      return next_el:get_attribute("class") or next_el:get_attribute("mathvariant")
-    end
-    local is_span = function(next_el)
-      return charclasses[get_name(next_el)]
-    end
-
-    local function get_next(curr, class)
-      local next_el = curr:get_next_node()
-      if next_el and next_el:is_element() and is_span(next_el) then
-        return next_el
-        -- if the next node is space followed by a matching element, we should add this space
-      elseif next_el and next_el:is_text() and get_next(next_el, class) then
-        local text = next_el._text 
-        -- match only text containing just whitespace
-        if text:match("^%s+$") then return next_el end
-      end
-    end
     -- loop over all elements and test if the current element is in a list of
     -- processed elements (charclasses)
     if is_span(el) then
       local next_el = get_next(el)
       -- loop over the following elements and test whether they are of the same type
       -- as the current one
-      while  next_el do
+      while next_el do
         -- save the next element because we will remove it later
         local real_next = get_next(next_el)
         if get_name(el) == get_name(next_el) and has_matching_attributes(el,next_el) and not el:get_attribute("id") then
-          -- it the following element match, copy it's children to the current element
-          for _, child in ipairs(next_el:get_children()) do
-            el:add_child_node(child)
-          end
-          -- remove the next element
-          next_el:remove_node()
+          join_elements(el, next_el)
           -- add the whitespace
         elseif next_el:is_text() then
           local s = next_el._text
@@ -109,6 +104,36 @@ local function join_characters(obj,par)
       end
     end
 
+  end)
+  -- process <mi> elements
+  obj:traverse_elements(function(el)
+    local function get_next_mi(curr)
+      local next_el = curr:get_next_node()
+      if next_el and next_el:is_element() then
+        return next_el
+      end
+    end
+    local function has_no_attributes(x)
+      return table_count(x._attr) == 0
+    end
+
+    -- join only subsequential <mi> elements with no attributes
+    if get_name(el) == "mi" and has_no_attributes(el) then
+      local next_el = get_next_mi(el)
+      while next_el do
+        local real_next = get_next_mi(next_el)
+        if get_name(next_el) == "mi" and has_no_attributes(next_el) then
+          join_elements(el, next_el)
+          -- set math variant to italic 
+          -- (if the parent <mstyle> element doesn't set it to something else)
+          update_mathvariant(el)
+        else
+          -- break the loop otherwise
+          real_next = nil
+        end
+        next_el = real_next
+      end
+    end
   end)
   -- join text nodes in an element into one
   obj:traverse_elements(function(el)
