155b334a387e998bb028ec44c4a76e2ff8ba9506
Michal Hoftich <michal.h21@gmail.com>
2021-09-08 16:36:35 +0200

    starting support for image dimensions

--- domfilters/make4ht-odtsvg.lua
+++ domfilters/make4ht-odtsvg.lua
@@ -2,6 +2,7 @@
 local log = logging.new "odtsvg"
 local function get_svg_dimensions(filename) 
   local width, height
+  log:debug("file exists", filename, mkutils.file_exists(filename))
   if mkutils.file_exists(filename) then 
     for line in io.lines(filename) do
       width = line:match("width%s*=%s*[\"'](.-)[\"']") or width
@@ -13,25 +14,27 @@ local function get_svg_dimensions(filename)
   return width, height
 end
 
+
 -- process 
 return function(dom)
   for _, pic in ipairs(dom:query_selector("draw|image")) do
     local imagename = pic:get_attribute("xlink:href")
     -- update SVG images dimensions
-    if imagename:match("svg$") then
-      log:debug("image", imagename)
-      local parent = pic:get_parent()
-      local width =  parent:get_attribute("svg:width")
-      local height = parent:get_attribute("svg:height")
-      -- if width == "0.0pt" then width = nil end
-      -- if height == "0.0pt" then height = nil end
-      if not width or not height then
+    log:debug("image", imagename)
+    local parent = pic:get_parent()
+    local width =  parent:get_attribute("svg:width")
+    local height = parent:get_attribute("svg:height")
+    -- if width == "0.0pt" then width = nil end
+    -- if height == "0.0pt" then height = nil end
+    if not width or not height then
+      if imagename:match("svg$") then
         width, height = get_svg_dimensions(imagename) --  or width, height
+      elseif imagename:match("png$") or imagename:match("jpe?g$") then
       end
-      log:debug("dimensions", width, height)
-      parent:set_attribute("svg:width", width)
-      parent:set_attribute("svg:height", height)
     end
+    log:debug("dimensions", width, height)
+    parent:set_attribute("svg:width", width)
+    parent:set_attribute("svg:height", height)
     -- if 
   end
   return dom
