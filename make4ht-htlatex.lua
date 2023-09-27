423f64bb763918b8b504780c9553974228af7c3a
Michal Hoftich <michal.h21@gmail.com>
2023-09-27 12:14:28 +0200

    use build_dir_arg in httex

--- make4ht-htlatex.lua
+++ make4ht-htlatex.lua
@@ -53,7 +53,7 @@ Make.latex_command = "${htlatex} --interaction=${interaction} ${build_dir_arg} $
 "\\documentstyle[tex4ht]}}\\RequirePackage[#1,html]{tex4ht}${packages}}\\makeatother\\HCode ${tex4ht_sty_par}.a.b.c."..
 "\\input \"\\detokenize{${tex_file}}\"'"
 
-Make.plain_command = '${htlatex} --interaction=${interaction} ${build_dir} ${latex_par}' ..
+Make.plain_command = '${htlatex} --interaction=${interaction} ${build_dir_arg} ${latex_par}' ..
 "'\\def\\Link#1.a.b.c.{\\expandafter\\def\\csname tex4ht\\endcsname{\\expandafter\\def\\csname tex4ht\\endcsname{#1,html}\\input tex4ht.sty }}" ..
 "\\def\\HCode{\\futurelet\\HCode\\HChar}\\def\\HChar{\\ifx\"\\HCode\\def\\HCode\"##1\"{\\Link##1}\\expandafter\\HCode\\else\\expandafter\\Link\\fi}" ..
 "\\HCode ${tex4ht_sty_par}.a.b.c.\\input \"\\detokenize{${tex_file}}\"'"
